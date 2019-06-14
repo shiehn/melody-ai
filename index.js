9c88bfade666ed36b1d937be3914d5f0db862b6c
steve hiehn <stevehiehn@hackcapital.com>
2019-06-14 14:49:32 -0700

    almost

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { sdk, ux } = require("@cto.ai/sdk");
 
 async function main() {
+  let afterCount = 1;
   const listQuestions = [
     {
       type: "list",
       name: "list",
-      message: `\nWhat impact is the incident having ${ux.colors.reset.green(
+      message: `\nSELECT CHORD NUMBER ${afterCount} ${ux.colors.reset.green(
         "→"
       )}`,
       choices: [
@@ -24,32 +25,66 @@ async function main() {
         "Fmin",
         "Gmin"
       ],
-      afterMessage: `${ux.colors.reset.green("✓")} Incident added!`
+      afterMessage: `${ux.colors.reset.green(
+        "✓"
+      )} Chord ${afterCount} slelected!`
     }
   ];
 
   let chordSequence = [];
 
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 8; i++) {
     const { list } = await ux.prompt(listQuestions);
 
     chordSequence.push(list);
+
+    afterCount++;
   }
 
-  console.log("OUTPUT", chordSequence);
-
-  // sdk.exec(
-  //   "java -jar /chords-to-melody-generator/target/chords-to-melody-generator-1.5.1.RELEASE.jar -mode=melody -chords=31^313*313*313*313*613*613*715*715*"
-  // );
-
   /*
-  const res = await sdk.user().catch(err => console.log(err));
-  const person = res && res.me ? `, ${res.me.username}` : " there";
-  const greeting = `\n👋 Hi${person}! This template will run the demo for CTO.ai CLI SDK where you'll see some basic interactions that are included. This will allow you to easily create a simple-to-use automation.`;
+  NOTES: 
+0 | Rest
+1 | a
+2 | b
+3 | c
+4 | d
+5 | e
+6 | f
+7 | g
 
-  sdk.log(greeting);
+  FLAT/NATURAL/SHARP
+0 - FLAT | 1 - NATURAL | 2 - SHARP
+
+  CHORD TYPES
+0 | maj
+1 | min
 */
-  //require("./demo");
+
+  let seq = "31^" + chordSequence.join("-");
+  let formatedSeq = seq
+    .replace("Amaj", "110")
+    .replace("Bmaj", "210")
+    .replace("Cmaj", "310")
+    .replace("Dmaj", "410")
+    .replace("Emaj", "510")
+    .replace("Fmaj", "610")
+    .replace("Gmaj", "710")
+
+    .replace("Amin", "111")
+    .replace("Bmin", "211")
+    .replace("Cmin", "311")
+    .replace("Dmin", "411")
+    .replace("Emin", "511")
+    .replace("Fmin", "611")
+    .replace("Gmin", "711");
+
+  console.log("OUTPUT", formatedSeq);
+
+  console.log("GENERATING 10 MELODIES!!");
+
+  sdk.exec(
+    "java -jar /chords-to-melody-generator/target/chords-to-melody-generator-1.5.1.RELEASE.jar -mode=melody -chords=31^313*313*313*313*613*613*715*715*"
+  );
 }
 
 main();
